c35e6f0d37adbf6069d683d1f67082690231e1c6
P.E. Butler III <p.e.butler3@gmail.com>
2024-04-16 21:56:42 -0600

    set sessionId from route

--- composables/useCreateUser.js
+++ composables/useCreateUser.js
@@ -1,14 +1,16 @@
 import { useSession } from '../stores/session';
 
 export const useCreateUser = async () => {
+  const router = useRouter();
   const supabase = useSupabaseClient();
   const store = useSession();
+  const sessionId = router.currentRoute.value.params.id[0];
 
   const { data, error } = await supabase
     .from('users')
     .insert({
         username: store.sessionUsername,
-        session_id: store.temporarySession.id,
+        session_id: sessionId || store.temporarySession.id,
         observer: store.sessionObserver,
       })
     .select();
